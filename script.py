a61ded20b04523bad0554a0126f1e96217b83455
Wild Tangent <wildtangent4@gmail.com>
2024-11-25 17:08:52 +0000

    implemented google search API to automatically perform the generated searches

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 fields = [
     "Cyber security",
@@ -41,6 +42,8 @@ search_terms = [
 ]
 
 API_KEY = os.environ.get("GOOGLE_SEARCH_API_KEY", "set the environment variable GOOGLE_SEARCH_API_KEY to your project's API key")
+search_engine_ID= "5287871e9271b4d4d"
+
 all_generated_searches = []
 
 for field in fields:
@@ -50,6 +53,21 @@ for field in fields:
                 field + " " + search_term + " in " + location
             )
 
+cached_searches = []
+cached_results = []
 
-for search in all_generated_searches:
+
+
+start = int(input("start from search number: "))-1
+for i in range(start,len(all_generated_searches)):
+    search = all_generated_searches[i]
     print(search)
+    response = requests.get("https://customsearch.googleapis.com/customsearch/v1?key="+API_KEY+"&cx="+search_engine_ID+"&q="+search).json()
+
+    for item in response["items"]:
+        if item["link"] not in cached_results:#dont bother outputting websites I have already seen this session
+            cached_results.append(item["link"])
+            print(item["title"])
+            print(item["link"])
+
+    input("next search")
