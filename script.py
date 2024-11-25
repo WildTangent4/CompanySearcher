d1cfe1394d3a3b899a322be77259402878b41d4c
Wild Tangent <wildtangent4@gmail.com>
2024-11-25 17:15:11 +0000

    added filtering to exclude duplicate search results

--- script.py
+++ script.py
@@ -61,13 +61,18 @@ cached_results = []
 start = int(input("start from search number: "))-1
 for i in range(start,len(all_generated_searches)):
     search = all_generated_searches[i]
-    print(search)
+    print("\n"+search)
     response = requests.get("https://customsearch.googleapis.com/customsearch/v1?key="+API_KEY+"&cx="+search_engine_ID+"&q="+search).json()
 
     for item in response["items"]:
+        num_excluded =  0
         if item["link"] not in cached_results:#dont bother outputting websites I have already seen this session
             cached_results.append(item["link"])
             print(item["title"])
             print(item["link"])
+        else:
+            num_excluded+=1
+    if num_excluded>0:
+        print("excluded", str(num_excluded), "results for being duplicates")
 
-    input("next search")
+    input("end of search " + str(i+1) + ", press enter for next search")
