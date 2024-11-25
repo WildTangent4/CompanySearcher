cc470e4967c38c282e016708c35d176d4c3acba6
Wild Tangent <wildtangent4@gmail.com>
2024-11-25 16:19:15 +0000

    added search terms array

--- script.py
+++ script.py
@@ -8,3 +8,20 @@ fields = [
     "Banking",
     "Games"
 ]
+
+#note: since I am targeting SMEs which may not have good SEO, im using small regions
+locations = [
+    "North London",
+    "London",
+    "Midlands",
+    "Nottingham",
+    "Sheffield",
+    "Leicester",
+    "Exeter",
+    "Manchester",
+    "Liverpool",
+    "Stockport",
+    "Devon",
+    "UK" #might catch companies the smaller regions dont
+]
+
